7bb7b8cca097b779cf47df2545e75b48b586cf43
bfrest <freston75@gmail.com>
2018-12-14 05:04:42 -0700

    put a build together

---
+++ build/precache-manifest.5a04da826ef8415634f4a8ad4635a797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba6099cf9008a6c6f359",
+    "url": "/static/js/main.ba6099cf.chunk.js"
+  },
+  {
+    "revision": "78eb3d8b0d0c9235b92b",
+    "url": "/static/js/1.78eb3d8b.chunk.js"
+  },
+  {
+    "revision": "ba6099cf9008a6c6f359",
+    "url": "/static/css/main.5d4f822a.chunk.css"
+  },
+  {
+    "revision": "ad3c4831bb2127c9573814e975115b14",
+    "url": "/index.html"
+  }
+];
