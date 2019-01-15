2e9e53e2a6f0b220edf85002e37a39f0b68dac24
bfrest <freston75@gmail.com>
2019-01-14 18:09:44 -0700

    looks better

--- src/components/Extras.js
+++ src/components/Extras.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 import styled, { keyframes } from "styled-components";
 import ThemePicker from "./ThemePicker";
 
-const comeIn = keyframes`
-0%{transform: scale(.4); opacity: 0;}
-60%{ transform: scale(1.06);}
-100%{transform: scale(1); opacity: 1}
+const show = keyframes`
+  from{ {transform: scale(.3)}}
+  to{ pink; {transform: scale(1)}}
 `;
 
-const goOut = keyframes`
-0%{transform: scale(1); right: 0;}
-100%{transform: scale(.3); right: -100vw;}
+const hide = keyframes`
+  from{{transform: scale(1)}}
+  to{background: pink; {transform: scale(.3)}}
 `;
 
 const ExtraStuff = styled.div`
   position: fixed;
   top: 0;
   right: 0vw;
-  opacity: 1;
   margin: 20px 10px 0 0;
   display: flex;
   flex-direction: column;
@@ -30,10 +28,9 @@ const ExtraStuff = styled.div`
   border: 1px solid rgba(191, 191, 191, 0.9);
   border-radius: 10px;
   box-shadow: 0px 0px 5px gray;
-  transition: all ease-in-out;
-  animation: ${comeIn};
-  animation-duration: 0.5s;
-  line-height: 3em;
+  transition: all 2s;
+  animation: ${show};
+  animation-duration: 0.3s;
 
   h1 {
     margin: -10px 0 30px 0;
@@ -59,19 +56,47 @@ const ExtraStuff = styled.div`
   }
 `;
 
+const Stuff = styled.div`
+  width: 100vw;
+  height: 100%;
+  background: rgba(3, 3, 3, 0.75);
+  z-index: 2;
+  position: absolute;
+`;
+
 class Extras extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amIShowing: null
+    };
+
+    this.modalRef = React.createRef();
+    this.addModal = this.addModal.bind(this);
+  }
+  componentDidMount() {
+    this.setState({ amIShowing: this.props.showSettings });
+    this.addModal();
+  }
+
+  addModal() {
+    const modal = this.modalRef;
+    modal.current.style.animation = "";
+  }
+
   render() {
     return (
-      <ExtraStuff>
-        <h1>Settings</h1>
-
-        {/* This is the x to close the modal */}
-        <svg onClick={() => this.props.handleSetting()} width="36" height="36" viewBox="0 0 263 264" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect x="0.331665" y="43.9653" width="61" height="310" rx="16" transform="rotate(-45 0.331665 43.9653)" />
-          <rect x="219.535" y="0.831757" width="61" height="310" rx="16" transform="rotate(45 219.535 0.831757)" />
-        </svg>
-        <ThemePicker handleTheme={this.props.handleTheme} />
-      </ExtraStuff>
+      <Stuff>
+        <ExtraStuff ref={this.modalRef}>
+          <h1>Settings</h1>
+          {/* This is the x to close the modal */}
+          <svg onClick={() => this.props.handleSetting()} width="36" height="36" viewBox="0 0 263 264" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect x="0.331665" y="43.9653" width="61" height="310" rx="16" transform="rotate(-45 0.331665 43.9653)" />
+            <rect x="219.535" y="0.831757" width="61" height="310" rx="16" transform="rotate(45 219.535 0.831757)" />
+          </svg>
+          <ThemePicker handleTheme={this.props.handleTheme} />
+        </ExtraStuff>
+      </Stuff>
     );
   }
 }
