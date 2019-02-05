4af49d5de7d82ff0b0a26d3cdbf04282e62cbcc0
bfrest <freston75@gmail.com>
2019-02-05 06:33:44 -0700

    fixed broken image links

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,25 +5,25 @@ class TechList extends Component {
     return (
       <div className="tech-logos">
         <a href="https://reactjs.org/">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/react.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/react.svg" alt="logo" className="logos" />
         </a>
 
         <a href="https://nodejs.org/en/">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/nodejs-seeklogo.com.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/nodejs-seeklogo.com.svg" alt="logo" className="logos" />
         </a>
 
         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/javascript-js-seeklogo.com.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/javascript-js-seeklogo.com.svg" alt="logo" className="logos" />
         </a>
 
         <a href="https://redux.js.org/">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/redux-seeklogo.com.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/redux-seeklogo.com.svg" alt="logo" className="logos" />
         </a>
         <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/html5-with-wordmark-color.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/html5-with-wordmark-color.svg" alt="logo" className="logos" />
         </a>
         <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/css3-seeklogo.com.svg" alt="logo" className="logos" />
+          <img src="https://res.cloudinary.com/bfrest/image/upload/v1541639622/blog/css3-seeklogo.com.svg" alt="logo" className="logos" />
         </a>
       </div>
     );
