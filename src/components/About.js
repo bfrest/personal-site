cc86efb6a78bd7727216c598b32478c2b99cfe63
bfrest <freston75@gmail.com>
2019-02-23 08:13:56 -0700

    got the projects section spiffed up

--- src/components/About.js
+++ src/components/About.js
@@ -18,34 +18,20 @@ const FullScreen = styled.div`
     right: 0;
     margin-right: -25px;
   }
-`;
 
-const BlueBackGround = styled.div`
-  background: #7e57c2;
-  width: 100%;
-  height: 100%;
-  color: #f2f2f2;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 25px;
-
-  h1 {
+  .intro {
+    background: #7e57c2;
+    color: #fff;
     font-size: 1.3em;
-  }
-
-  p {
-    width: 80%;
-    text-align: left;
-    font-size: 1em;
-    line-height: 1.9em;
+    height: 60vh;
+    padding: 20px;
   }
 `;
 
 const Created = styled.div`
-  margin-top: 80px;
   display: flex;
   flex-direction: column;
+  margin-top: 200px;
 
   h2 {
     font-weight: 900;
@@ -54,70 +40,78 @@ const Created = styled.div`
     text-decoration-color: #00d2d3;
   }
 
-  .box {
-    width: 50px;
-    height: 50px;
-    background: #7e57c2;
-  }
-
   img {
     width: 100px;
   }
 `;
 
-const AboutMe = styled.div`
-  color: #00d2d3;
-
-  h1 {
-    text-decoration-line: underline;
-    text-decoration-color: #7e57c2;
-  }
-
-  p {
-    color: #7e57c2;
-  }
-`;
-
 const ListOfStuff = styled.div`
   width: 100vw;
-  display: grid;
+  display: flex;
+  flex-direction: column;
   justify-items: center;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 20px;
+  align-items: center;
+  font-size: 1.3em;
+
+  .project {
+    height: 
+    width: 80vw;
+    text-align: left;
+    margin: 30px;
+
+    img {
+      width: 100%;
+      box-shadow: 3px 3px 5px #555;
+      margin: 25px 0;
+    }
+  }
 `;
 
 class About extends Component {
   render() {
     return (
       <FullScreen>
-        <BlueBackGround>
+        <div className="intro">
           <h1>Hey There!</h1>
           <p>
-            My Name is Brendon Freston and I am a front end developer who values accesibility, creativity, and an innovative culture. I enjoy building useful, beautiful and fast
-            digital products.
+            My Name is Brendon and I am a front end developer who values accesibility, creativity, and an innovative culture. I enjoy building useful, beautiful and fast digital
+            products.
           </p>
-        </BlueBackGround>
+        </div>
 
         <Created>
           <h2>What I've Created</h2>
           <p>Here are some of the things I have built in the past.</p>
 
           <ListOfStuff>
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
-            <div className="box" />
+            <div className="project">
+              <h2>Slack Clone</h2>
+              <img src="https://res.cloudinary.com/bfrest/image/upload/v1550933164/blog/Screen_Shot_2019-02-23_at_7.45.05_AM.png" />
+              <p>
+                This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This
+                slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl
+              </p>
+            </div>
+
+            <div className="project">
+              <h2>Deathwish Coffee Clone</h2>
+              <img src="https://res.cloudinary.com/bfrest/image/upload/v1550933167/blog/Screen_Shot_2019-02-23_at_7.07.29_AM.png" />
+              <p>
+                This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This
+                slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl
+              </p>
+            </div>
+
+            <div className="project">
+              <h2>Foxridge Development</h2>
+              <img src="https://res.cloudinary.com/bfrest/image/upload/v1550933205/blog/Screen_Shot_2019-02-23_at_7.22.33_AM.png" />
+              <p>
+                This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This
+                slack clonse was a clonse of slacl This slack clonse was a clonse of slacl This slack clonse was a clonse of sla
+              </p>
+            </div>
           </ListOfStuff>
         </Created>
-
-        <AboutMe>
-          <h1>About Me</h1>
-          <p>I love dogs</p>
-        </AboutMe>
       </FullScreen>
     );
   }
